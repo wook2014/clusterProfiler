335235254697dee30e17ac1be1a2d0dd824f46ca
Guangchuang Yu <guangchuangyu@gmail.com>
2024-06-15 17:47:38 +0800

    update

--- R/dplyr-group_by.R
+++ R/dplyr-group_by.R
@@ -1,9 +1,9 @@
 
 ##' @method group_by enrichResult
 ##' @export
-group_by.enrichResult <- function(.data, ..., add = FALSE, .drop = FALSE) {
+group_by.enrichResult <- function(.data, ..., .add = FALSE, .drop = FALSE) {
     dots <- quos(...)
-    .data@result %<>% group_by(!!!dots, add = add, .drop = .drop)
+    .data@result %<>% group_by(!!!dots, .add = .add, .drop = .drop)
     return(.data)
 }
 
@@ -14,8 +14,8 @@ group_by.gseaResult <- group_by.enrichResult
 
 ##' @method group_by compareClusterResult
 ##' @export
-group_by.compareClusterResult <- function(.data, ..., add = FALSE, .drop = FALSE) {
+group_by.compareClusterResult <- function(.data, ..., .add = FALSE, .drop = FALSE) {
     dots <- quos(...)
-    .data@compareClusterResult %<>% group_by(!!!dots, add = add, .drop = .drop)
+    .data@compareClusterResult %<>% group_by(!!!dots, .add = .add, .drop = .drop)
     return(.data)
 }
